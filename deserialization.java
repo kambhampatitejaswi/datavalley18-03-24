8ce1c41ceabe34f229199e237e03578b7cc9d11a
kambhampatitejaswi <105159024+kambhampatitejaswi@users.noreply.github.com>
2024-03-18 17:31:03 +0530

    Create deserialization.java

---
+++ deserialization.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Product implements Serializable {
+    private int id;
+    private String name;
+    private String category;
+    private double price;
+
+    // Constructor, getters and setters
+
+    public static void main(String[] args) {
+        // Serialization
+        try {
+            Product product = new Product(1, "Phone", "Electronics", 999.99);
+            FileOutputStream fileOut = new FileOutputStream("product.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(product);
+            out.close();
+            fileOut.close();
+            System.out.println("Serialized data is saved in product.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+
+        // Deserialization
+        try {
+            FileInputStream fileIn = new FileInputStream("product.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Product product = (Product) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Deserialized Product:");
+            System.out.println("ID: " + product.getId());
+            System.out.println("Name: " + product.getName());
+            System.out.println("Category: " + product.getCategory());
+            System.out.println("Price: " + product.getPrice());
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Product class not found");
+            c.printStackTrace();
+        }
+    }
+}
